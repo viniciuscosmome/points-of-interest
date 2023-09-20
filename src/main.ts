e1e6c84536be4ebd2561b74d05495eaf3efdc526
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-09-20 18:55:04 -0300

    chore: config global validation pipes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule } from '@nestjs/swagger';
 
 import { PORT } from './globals/constants';
@@ -9,8 +10,10 @@ import { MainModule } from './main.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(MainModule);
 
+  app.useGlobalPipes(new ValidationPipe());
+
   const document = SwaggerModule.createDocument(app, swaggerDocumentConfig);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('', app, document);
 
   await app.listen(PORT, () => {
     console.info(`[ONN] Local port: ${PORT}`);
