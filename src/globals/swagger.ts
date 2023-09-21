7ba0b541b32d54d25946110effabc9c7408bbfe1
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-09-21 14:56:42 -0300

    docs: updates swagger

--- src/globals/swagger.ts
+++ src/globals/swagger.ts
@@ -1,10 +1,11 @@
 import { DocumentBuilder } from '@nestjs/swagger';
+import { APP_LICENSE, APP_VERSION } from './constants';
 
 export const swaggerDocumentConfig = new DocumentBuilder()
   .setTitle('Points Of Interest')
-  .setVersion('0.0.1')
+  .setVersion(APP_VERSION)
   .setLicense(
-    'MIT license',
+    `${APP_LICENSE} license`,
     'https://github.com/viniciuscosmome/points-of-interest/blob/main/LICENSE',
   )
   .build();
