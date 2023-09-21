9885269988a17e0ec75d1e04095cec95c461da7c
viniciuscosmome <48590313+viniciuscosmome@users.noreply.github.com>
2023-09-21 02:54:40 -0300

    docs: update tags

--- src/modules/points/points.controller.ts
+++ src/modules/points/points.controller.ts
@@ -4,17 +4,16 @@ import { ApiTags } from '@nestjs/swagger';
 import { PointsService } from './points.service';
 import { CreatePointOfInterestDto } from './points.dto';
 
-@Controller('point')
+@ApiTags('Handle points')
+@Controller('points')
 export class PointsController {
   constructor(private pointsService: PointsService) {}
 
-  @ApiTags('Handles points')
-  @Get('')
+  @Get()
   async getAllPoints() {
     return await this.pointsService.getAllPoints();
   }
 
-  @ApiTags('Handles points')
   @Post('creates')
   async createPoint(@Body() input: CreatePointOfInterestDto) {
     await this.pointsService.saveNewPont(input);
